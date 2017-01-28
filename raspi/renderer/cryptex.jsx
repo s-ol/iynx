63341e5bb15bf129429d68f989a05c6ce22db45e
s-ol <s-ol@users.noreply.github.com>
2017-01-28 23:20:03 +0100

    some stuff

--- raspi/renderer/cryptex.jsx
+++ raspi/renderer/cryptex.jsx
@@ -4,19 +4,6 @@ import memoize from 'memoization';
 
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ-'.split('');
 
-const Letter  = memoize(({ children, offset }) => (
-  <div
-    style={{
-      height: 40,
-      fontSize: '3em',
-      transition: 'transform 0.2s',
-    }}
-  >
-    {children}
-  </div>
-));
-
-
 const fillers = 2;
 const extendedAlphabet = [
 ...alphabet.slice(alphabet.length - fillers - 1, alphabet.length - 1),
@@ -28,15 +15,7 @@ const Wheel = memoize(({ offset }) => {
   const centered = offset + fillers;
 
   return (
-    <div style={{
-      position: 'relative',
-      display: 'inline-block',
-      width: 40,
-      height: 150,
-      margin: 10,
-      border: '1px solid #000',
-      overflow: 'hidden',
-    }}>
+    <div className="wheel">
       <div style={{
         position: 'absolute',
         display: 'flex',
@@ -45,8 +24,8 @@ const Wheel = memoize(({ offset }) => {
         top: centered * -40 + 52,
         transition: 'top 0.3s',
       }}>
-        {extendedAlphabet.map(
-          (character, index) => <Letter key={index} offset={index - centered}>{character}</Letter>
+        {extendedAlphabet.map((character, index) =>
+          <div key={index} className="letter">{character}</div>
         )}
       </div>
     </div>
@@ -85,21 +64,9 @@ class Cryptex extends React.Component {
     const { sliders } = this.state;
 
     return (
-      <div style={{
-        position: 'relative',
-        display: 'inline-block',
-      }}>
+      <div className="cryptex">
         {sliders.map((value, index) => <Wheel key={index} offset={value} />)}
-        <div style={{
-          position: 'absolute',
-          top: '50%',
-          left: 0,
-          right: 0,
-          height: 46,
-          marginTop: -23,
-          border: '4px solid #888',
-        }}>
-        </div>
+        <div className="visor" />
       </div>
     );
   }
