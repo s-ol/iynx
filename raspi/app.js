fcc311188f85c29c495a0c4de067fc31a1ff562f
s-ol <s-ol@users.noreply.github.com>
2017-01-29 23:37:56 +0100

    quickfix

--- raspi/app.js
+++ raspi/app.js
@@ -1,40 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Cryptex from './cryptex';
-import { decode, TemporaryStream, Queue } from './audio';
-import Speaker from 'speaker';
+// import { decode, TemporaryStream, Queue } from './audio';
+// import Speaker from 'speaker';
 
-const audio = Queue.to(new Speaker());
+// const audio = Queue.to(new Speaker());
+
+const Files = () => (<div>Files</div>);
 
 class App extends React.Component {
-  constructor() {
+  constructor () {
     super();
     this.state = {
       solved: false,
+      screen: 'personal',
     };
   }
 
   componentWillUpdate(props, { solved }) {
-    if (solved && !this.state.solved)
-      decode('piano2.wav').then(stream => audio.append(stream.pipe(new TemporaryStream(200))));
+    // if (solved && !this.state.solved)
+    //  decode('piano2.wav').then(stream => audio.append(stream.pipe(new TemporaryStream(200))));
   }
 
   render() {
-    const { solved } = this.state;
+    const { screen } = this.state;
 
-    return (
-      <div>
-        <h1>IYNX</h1>
-        {solved && 'thanks man'}
-        <div>
+    let content = null;
+    switch (screen) {
+      case 'system':
+        content = (
           <Cryptex
             secret="HELP--ME"
             onChanged={solved => this.setState({ solved })}
           />
-        </div>
+        );
+        break;
+      case 'personal':
+        return (
+          <Files />
+        );
+        break;
+      case 'work':
+        break;
+      case 'gallery':
+        break;
+    }
+
+    return (
+      <div>
+        {content}
       </div>
     );
   }
 }
 
-window.onload = () => ReactDOM.render(<App />, document.getElementById('view'));
+window.onload = () =>
+  ReactDOM.render(<App />, document.getElementById('view'));
